f44628bf12062d19ce0e97a39e69abcc43da3143
lorenatoscano <lorenatoscano@ufrn.edu.br>
2021-10-12 12:07:02 -0300

    Adiciona botões de marcar como respondida e dar destaque a pergunta

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from 'react';
+import cx from 'classnames';
+
 import '../styles/question.scss';
 
 type QuestionProps = {
@@ -8,15 +10,25 @@ type QuestionProps = {
     avatar: string;
   };
   children?: ReactNode,
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 }
 
 export function Question({
   content,
   author,
-  children
+  isAnswered = false,
+  isHighlighted = false,
+  children,
 }: QuestionProps) {
   return (
-    <div className="question">
+    <div
+      className={cx(
+        'question',
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered },
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
