42aebee75d88b910cad3fa565021144666a0fb9e
lorenatoscano <lorenatoscano@ufrn.edu.br>
2021-10-12 14:34:38 -0300

    Adiciona aviso com ilustração quando não há perguntas nas telas de admin e de participante

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -4,11 +4,11 @@ import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
 import checkImg from '../assets/images/check.svg';
 import answerImg from '../assets/images/answer.svg';
+import emptyImg from '../assets/images/empty-questions.svg';
 
 import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
-// import { useAuth } from '../hooks/useAuth';
 import { useRoom } from '../hooks/useRoom';
 import { database } from '../services/firebase';
 
@@ -19,7 +19,6 @@ type RoomParams = {
 }
 
 export function AdminRoom() {
-  // const { user } = useAuth();
   const history = useHistory();
   const params = useParams<RoomParams>();
   const roomId = params.id;
@@ -27,9 +26,11 @@ export function AdminRoom() {
   const { title, questions } = useRoom(roomId);
 
   async function handleEndRoom() {
-    await database.ref(`rooms/${roomId}`).update({
-      endedAt: new Date(),
-    });
+    if (window.confirm('Tem certeza que você deseja encerrar esta sala?')) {
+      await database.ref(`rooms/${roomId}`).update({
+        endedAt: new Date(),
+      });
+    }
 
     history.push('/');
   }
@@ -70,42 +71,53 @@ export function AdminRoom() {
           {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
 
-        <div className="question-list">
-          {questions.map(question => {
-            return (
-              <Question
-                key={question.id}
-                content={question.content}
-                author={question.author}
-                isAnswered={question.isAnswered}
-                isHighlighted={question.isHighlighted}
-              >
-                {!question.isAnswered && (
-                  <>
+        {questions.length > 0
+          ? (
+            <div className="question-list">
+              {questions.map(question => {
+                return (
+                  <Question
+                    key={question.id}
+                    content={question.content}
+                    author={question.author}
+                    isAnswered={question.isAnswered}
+                    isHighlighted={question.isHighlighted}
+                  >
+                    {!question.isAnswered && (
+                      <>
+                        <button
+                          type="button"
+                          onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                        >
+                          <img src={checkImg} alt="Marcar pergunta como respondida" />
+                        </button>
+                        <button
+                          type="button"
+                          onClick={() => handleHighlightQuestion(question.id)}
+                        >
+                          <img src={answerImg} alt="Dar destaque à pergunta" />
+                        </button>
+                      </>
+                    )}
                     <button
                       type="button"
-                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                      onClick={() => handleDeleteQuestion(question.id)}
                     >
-                      <img src={checkImg} alt="Marcar pergunta como respondida" />
+                      <img src={deleteImg} alt="Remover pergunta" />
                     </button>
-                    <button
-                      type="button"
-                      onClick={() => handleHighlightQuestion(question.id)}
-                    >
-                      <img src={answerImg} alt="Dar destaque à pergunta" />
-                    </button>
-                  </>
-                )}
-                <button
-                  type="button"
-                  onClick={() => handleDeleteQuestion(question.id)}
-                >
-                  <img src={deleteImg} alt="Remover pergunta" />
-                </button>
-              </Question>
-            );
-          })}
-        </div>
+                  </Question>
+                );
+              })}
+            </div>
+          )
+          : (
+            <div className="empty-questions">
+              <img src={emptyImg} alt="Ilustração simbolizando perguntas" />
+              <h2>Nenhuma pergunta por aqui...</h2>
+              <p>Envie o código desta sala para seus amigos e comece a responder perguntas!</p>
+            </div>
+          )
+        }
       </main>
     </div>
   );
